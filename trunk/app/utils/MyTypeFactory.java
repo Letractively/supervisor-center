df887eb27bf4774569ffaa5c056888a63ab22a7e
bigjimlefou@gmail.com <bigjimlefou@gmail.com@68f01507-24ab-8983-5346-3e21dcbbbcf9>
2012-11-27 10:01:22 +0000

    [TRUNK] Supervisor xmlrpc classes implementation

---
+++ trunk/app/utils/MyTypeFactory.java
@@ -0,0 +1,31 @@
+package be.bigjimnetwork.testxmlrpc;
+
+import org.apache.xmlrpc.common.TypeFactoryImpl;
+import org.apache.xmlrpc.common.XmlRpcController;
+import org.apache.xmlrpc.common.XmlRpcStreamConfig;
+import org.apache.xmlrpc.serializer.StringSerializer;
+import org.apache.xmlrpc.serializer.TypeSerializer;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+public class MyTypeFactory extends TypeFactoryImpl {
+
+    private static final TypeSerializer myStringSerializer =
+            new StringSerializer() {
+                public void write(ContentHandler pHandler, Object pObject) throws SAXException {
+                    write(pHandler, STRING_TAG, pObject.toString());
+                }
+            };
+
+    public MyTypeFactory(XmlRpcController pController) {
+        super(pController);
+    }
+
+    public TypeSerializer getSerializer(XmlRpcStreamConfig pConfig,
+            Object pObject) throws SAXException {
+        if (pObject instanceof String) {
+            return myStringSerializer;
+        }
+        return super.getSerializer(pConfig, pObject);
+    }
+}
---
+++ trunk/app/utils/ProcessInfo.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.bigjimnetwork.testxmlrpc;
+
+import java.util.Map;
+
+/**
+ *
+ * @author bigjim
+ */
+class ProcessInfo {
+    String name;
+    String group;
+    int start;
+    int stop;
+    int now;
+    int state;
+    String statename;
+    String spawnerr;
+    int exitstatus;
+    String stdout_logfile;
+    String stderr_logfile;
+    int pid;
+
+    public ProcessInfo(Map processInfo) {
+        this.name = (String) processInfo.get("name");
+        this.group = (String) processInfo.get("group");
+        this.start = (Integer) processInfo.get("start");
+        this.stop = (Integer) processInfo.get("stop");
+        this.now = (Integer) processInfo.get("now");
+        this.state = (Integer) processInfo.get("state");
+        this.statename = (String) processInfo.get("statename");
+        this.spawnerr = (String) processInfo.get("spawnerr");
+        this.exitstatus = (Integer) processInfo.get("exitstatus");
+        this.stdout_logfile = (String) processInfo.get("stdout_logfile");
+        this.stderr_logfile = (String) processInfo.get("stderr_logfile");
+        this.pid = (Integer) processInfo.get("pid");
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getStop() {
+        return stop;
+    }
+
+    public int getNow() {
+        return now;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public String getStatename() {
+        return statename;
+    }
+
+    public String getSpawnerr() {
+        return spawnerr;
+    }
+
+    public int getExitstatus() {
+        return exitstatus;
+    }
+
+    public String getStdout_logfile() {
+        return stdout_logfile;
+    }
+
+    public String getStderr_logfile() {
+        return stderr_logfile;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    @Override
+    public String toString() {
+        return "ProcessInfo{" + "name=" + name + ", group=" + group + ", start=" + start + ", stop=" + stop + ", now=" + now + ", state=" + state + ", statename=" + statename + ", spawnerr=" + spawnerr + ", exitstatus=" + exitstatus + ", stdout_logfile=" + stdout_logfile + ", stderr_logfile=" + stderr_logfile + ", pid=" + pid + '}';
+    }
+
+
+}
